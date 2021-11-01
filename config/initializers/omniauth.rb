45c263c28655e0b62e9fd67e274a70b894107091
nagasho1122 <nmoca112222@gmail.com>
2021-11-01 11:15:07 +0000

    setting omniauth second

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,Rails.application.credentials.dig(:google, :client_id),Rails.application.credentials.dig(:google, :client_secret)
+  provider :google_oauth2,
+           Rails.application.credentials.dig(:google, :client_id),
+           Rails.application.credentials.dig(:google, :client_secret)
 end
